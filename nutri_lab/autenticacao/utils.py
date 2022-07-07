ba8c31814540e27fbd391e2d2400deadf4cc3ed3
wesleyjw <jwesleybiologo@gmail.com>
2022-07-07 08:26:07 -0300

    Finaliza paginas logar e cadastro

--- nutri_lab/autenticacao/utils.py
+++ nutri_lab/autenticacao/utils.py
@@ -1,3 +1,7 @@
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+from django.utils.html import strip_tags
+from django.template.loader import render_to_string
 from email import message
 import re
 from django.contrib import messages
@@ -36,3 +40,16 @@ def password_is_valid(request, password, confirm_password):
         return False
 
     return True
+
+
+def email_html(path_template: str, assunto: str, para: list, **kwargs) -> dict:
+
+    html_content = render_to_string(path_template, kwargs)
+    text_content = strip_tags(html_content)
+
+    email = EmailMultiAlternatives(
+        assunto, text_content, settings.EMAIL_HOST_USER, para)
+
+    email.attach_alternative(html_content, "text/html")
+    email.send()
+    return {'status': 1}
--- nutri_lab/autenticacao/views.py
+++ nutri_lab/autenticacao/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages, auth
 from django.contrib.messages import constants
-
-from .utils import password_is_valid
+import os
+from django.conf import settings
+from .models import Ativacao
+from .utils import password_is_valid, email_html
+from hashlib import sha256
 
 # Create your views here.
 
 
 def cadastro(request):
     if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('/')
         return render(request, 'cadastro.html')
     elif request.method == 'POST':
         username = request.POST.get('usuario')
@@ -27,6 +32,14 @@ def cadastro(request):
                                             password=senha,
                                             is_active=False)
             user.save()
+            token = sha256(f"{username}{email}".encode()).hexdigest()
+            ativacao = Ativacao(token=token, user=user)
+            ativacao.save()
+            path_template = os.path.join(
+                settings.BASE_DIR, 'autenticacao/templates/emails/cadastro_confirmado.html')
+            email_html(path_template, 'Cadastro confirmado',
+                       [email, ], username=username,
+                       link_ativacao=f"127.0.0.1:8000/auth/ativar_conta/{token}")
             messages.add_message(request, constants.SUCCESS,
                                  'Usuário cadastrado com sucesso!')
             return redirect('/auth/logar')
@@ -38,6 +51,8 @@ def cadastro(request):
 
 def logar(request):
     if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
         return render(request, 'logar.html')
     elif request.method == "POST":
 
@@ -52,3 +67,23 @@ def logar(request):
         else:
             auth.login(request, usuario)
             return redirect('/')
+
+
+def sair(request):
+    auth.logout(request)
+    return redirect('/auth/logar')
+
+
+def ativar_conta(request, token):
+    token = get_object_or_404(Ativacao, token=token)
+    if token.ativo:
+        messages.add_message(request, constants.WARNING,
+                             'Essa token já foi usado')
+        return redirect('/auth/logar')
+    user = User.objects.get(username=token.user.username)
+    user.is_active = True
+    user.save()
+    token.ativo = True
+    token.save()
+    messages.add_message(request, constants.SUCCESS, 'Conta ativa com sucesso')
+    return redirect('/auth/logar')
